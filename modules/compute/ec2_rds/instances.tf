cccea1b3f8504ce1128b7e19a7ade774601bf73f
harikv23 <59342438+harikv23@users.noreply.github.com>
2020-01-13 18:47:18 +0530

    Update instances.tf

--- modules/compute/ec2_rds/instances.tf
+++ modules/compute/ec2_rds/instances.tf
@@ -2,22 +2,22 @@ resource "aws_instance" "web" {
   count 	= "${var.ec2_count}"
   ami           = "${var.ami_id}"
   instance_type = "${var.instance_type}"
-  #subnet_id     = "${var.subnet_id}"
+  subnet_id     = "${var.subnet_id}"
 
   tags = {
     Name = "MyApp1"
   }
 }
 
-resource "aws_db_instance" "default" {
-  allocated_storage    = "${var.allocated_storage}"
-  storage_type         = "${var.storage_type}"
-  engine               = "${var.engine}"
-  engine_version       = "${var.engine_version}"
-  instance_class       = "${var.instance_class}"
-  name                 = "${var.db_name}"
-  username             = "${var.username}"
-  password             = "${var.password}"
-  parameter_group_name = "${var.parameter_group_name}"
-}
+#resource "aws_db_instance" "default" {
+#  allocated_storage    = "${var.allocated_storage}"
+#  storage_type         = "${var.storage_type}"
+#  engine               = "${var.engine}"
+#  engine_version       = "${var.engine_version}"
+#  instance_class       = "${var.instance_class}"
+#  name                 = "${var.db_name}"
+#  username             = "${var.username}"
+#  password             = "${var.password}"
+#  parameter_group_name = "${var.parameter_group_name}"
+#}
 
