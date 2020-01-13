7c8852b462c92c4633c3efa8d140972181a6fe39
harikv23 <59342438+harikv23@users.noreply.github.com>
2020-01-13 18:31:45 +0530

    Update main.tf

--- np_account/ap-south-1/dev_vpc/c2o/ec2/main.tf
+++ np_account/ap-south-1/dev_vpc/c2o/ec2/main.tf
@@ -1,14 +1,15 @@
 provider "aws" {
  region = "ap-south-1"
 } 
-module "my_vpc" {
- source 	= "../../../../../modules/network/vpc"
- subnet_id 	= "${var.subnet_id}"	
-}
+#module "my_vpc" {
+# source 	= "../../../../../modules/network/vpc"
+# subnet_id 	= "${var.subnet_id}"	
+#}
 
 module "my_ec2" {
  source 	= "../../../../../modules/compute/ec2_rds"
  ami_id 	= "${var.ami_id}"
- subnet_id      = "{var.subnet_id}"
+ subnet_id = "{var.subnet_id}"
+ key_name = "${var.key_name}"
  password	= "${var.password}"
 }
